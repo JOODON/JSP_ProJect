1df6a846f2aef9cbfe79030b81fec05f057308fe
주동호 <100278301+JOODON@users.noreply.github.com>
2022-06-08 21:12:46 +0900

    06.08 JSP

--- src/main/java/com/example/jsp_project/HelloWorld.java
+++ src/main/java/com/example/jsp_project/HelloWorld.java
@@ -4,8 +4,8 @@ import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-@WebServlet("/hello-servlet")
-public class HelloServlet extends HttpServlet {
+@WebServlet("/helloworld")
+public class HelloWorld extends HttpServlet {
     private String message;
     private String message2;//메세지라는 필드를 만들어줌
 
@@ -15,10 +15,10 @@ public class HelloServlet extends HttpServlet {
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        System.out.println("doGet");
         response.setContentType("text/html");//HTML파일로 응답을 해주겠다.
         // Hello
         PrintWriter out = response.getWriter();//웹 브라우저에서 출력하기 위핸 스트림
-
         out.println("<html><body>");//기본적인 HTML사용
         out.println("<h1>" + message + "</h1>");
         out.println("<h1>" + message2 + "</h1>");
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -5,9 +5,11 @@
     <title>JSP - Hello World</title>
 </head>
 <body>
-<h1><%= "Hello World!" %>
-</h1>
+
+<h1>HelloWorld~~</h1>
+
 <br/>
 <a href="hello-servlet">Hello Servlet</a>
+<a href="/hello-world">Hello World</a>
 </body>
 </html>
